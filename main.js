e4a2eef3c056b6586ec6783631e6e1993e55f252
yhammzy <yisaahmed04@gmail.com>
2022-11-19 12:32:35 -0800

    CFV

--- main.js
+++ main.js
@@ -16,6 +16,39 @@ icon.addEventListener('click', function() {
     count = 0
     return
 })
+
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in')
+
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -60% 0px"
+
+};
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries,
+    appearOnScroll
+    ) {
+        entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }  else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }   
+        });
+    },
+    appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+
+    sliders.forEach(slider => {
+        appearOnScroll.observe(slider);
+    });
 // button.addEventListener('click', function() {
 //     if (count == 0 ) {
 //         list.style.display = 'flex'
